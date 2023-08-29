638167fab73196287847ef363ef1df1942bab435
Himanshu Agrawal <Himanshuaggarwal273@gmail.com>
2023-08-29 15:54:50 +0530

    stream code

--- Controllers/stream.js
+++ Controllers/stream.js
@@ -1,9 +1,23 @@
 const fs = require('fs')
 
-fs.readFile('./readFile.pdf', function(error, data){
-    if(error){
-        console.log('error in reading file ::', error)
-    }
+// fs.readFile('./readFile.pdf', function(error, data){
+//     if(error){
+//         console.log('error in reading file ::', error)
+//     }
 
-    console.log('read Data ::', data.toString())
+//     console.log('read Data ::', data.toString())
+// })
+
+let rstream = fs.createReadStream('./readFile.pdf')
+let wstream = fs.createWriteStream('./writeFile.pdf')
+
+rstream.on('data', (chunkData)=>{
+    console.log('chunk data ::', chunkData.toString())
+})
+
+rstream.pipe(wstream)
+// rstream.pipe(wstream)
+
+rstream.on('error', (error)=>{
+    console.log('data ::', error)
 })
