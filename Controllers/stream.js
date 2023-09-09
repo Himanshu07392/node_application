0bfa29b25800453ee00857d18afb9de5667da68f
Himanshu Agrawal <Himanshuaggarwal273@gmail.com>
2023-09-09 10:00:31 +0530

    push async code

--- Controllers/stream.js
+++ Controllers/stream.js
@@ -1,23 +1,30 @@
 const fs = require('fs')
 
-// fs.readFile('./readFile.pdf', function(error, data){
-//     if(error){
-//         console.log('error in reading file ::', error)
-//     }
+console.time()
+// servant or helper
+fs.readFile('./readFile.pdf', function(error, data){
+    if(error){
+        console.log('error in reading file ::', error)
+    }
 
-//     console.log('read Data ::', data.toString())
+    console.log('read Data ::', data.toString())
+})
+
+let filedata = fs.readFileSync('./readFile.pdf')
+console.log('filedata ::', filedata.toString())
+
+console.timeEnd()
+
+// let rstream = fs.createReadStream('./readFile.pdf')
+// let wstream = fs.createWriteStream('./writeFile.pdf')
+
+// rstream.on('data', (chunkData)=>{
+//     console.log('chunk data ::', chunkData.toString())
 // })
 
-let rstream = fs.createReadStream('./readFile.pdf')
-let wstream = fs.createWriteStream('./writeFile.pdf')
-
-rstream.on('data', (chunkData)=>{
-    console.log('chunk data ::', chunkData.toString())
-})
-
-rstream.pipe(wstream)
 // rstream.pipe(wstream)
+// // rstream.pipe(wstream)
 
-rstream.on('error', (error)=>{
-    console.log('data ::', error)
-})
+// rstream.on('error', (error)=>{
+//     console.log('data ::', error)
+// })
---
+++ index.js
@@ -0,0 +1,22 @@
+// Synchronous and Asynchronous
+
+// Synchronous is exceuting the code in the sequence that It has been written.
+// Asynchronous is executing the code without the sequence and independent from each other.
+
+// synchronous code
+
+console.log('Hello')
+console.log('World')
+
+// send response to the client or to the frontend.
+
+// Asynchronous code
+
+// setTimeout = Asynchronous function or This is a example of high order function also.
+
+// function helloWorld(){
+//     console.log('Hello World')
+// }
+
+// setTimeout(helloWorld, 2000)
+// console.log('After setTimeout.')
